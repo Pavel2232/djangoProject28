c6096c24092c70e36da46961f4a8bc4546e62a71
Павел <king-he@mail.ru>
2022-12-13 01:33:21 +0300

    Initial commit

---
+++ ads/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Ad(models.Model):
+    name= models.CharField(max_length=100)
+    author= models.CharField(max_length=200)
+    price= models.IntegerField()
+    description= models.CharField(max_length=300)
+    address= models.CharField(max_length=100)
+    is_published= models.BooleanField()
+
+    def __str__(self):
+        return self.name
